acd09f016cf02c436101de375abaf3adfdb37dd9
terra w <119160304+terra-woo@users.noreply.github.com>
2024-05-07 23:02:42 +0900

    Title: 정수 선언하고 곱 출력, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240507/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str1 = "26";
+        String str2 = "5";
+        int i = Integer.parseInt(str1)*Integer.parseInt(str2);
+        System.out.print(str1+" * "+str2+" = "+i);
+    }
+}
